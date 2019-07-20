11a04f5aa7b0ca053d3223d9a1d3b6bdce240621
sudiptochatterje <46589756+sudiptochatterje@users.noreply.github.com>
2019-07-20 18:50:06 +0530

    Update Abc.java

--- initial/Abc.java
+++ initial/Abc.java
@@ -1,12 +1,12 @@
 public class Abc
 {
-public static void main(String argv[])
+public static void main(String args[])
 {           
 String s="";
 String s1="";
 //int a=20;
 s+=20; 
 s1+=20;
-System.out.println(s1==s);    
+System.out.println(s1==s);//output:-false    
 }
 }
